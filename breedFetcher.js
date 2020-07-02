cb2ee6af4170ba35c8c21a04b0fe812c68ef0b02
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-02 18:04:50 +0000

    fetch breed description using CatApi

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,7 @@ const request = require('request');
 const breed = process.argv[2];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response) => {
-  if (error){
+  if (error) {
     console.log(error);
   } else {
     const data = JSON.parse(response.body);
