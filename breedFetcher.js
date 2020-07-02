f976ec5c5cf66a724752d3b46eec8b84b49f1b1e
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-02 18:44:45 +0000

    added test case

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,16 @@
 const request = require('request');
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(response.body);
-    data.length === 0 ? console.log(`Could not find breed: ${breed}`) : console.log(data[0].description);
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      let result;
+      const data = JSON.parse(response.body);
+      data.length === 0 ? result = (`Could not find breed: ${breedName}`) : result = (data[0].description);
+      callback(null, result);
+    }
+  });
+};
 
-});
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,11 @@
+const breed = process.argv[2];
+
+const { fetchBreedDescription } = require('./breedFetcher');
+
+fetchBreedDescription(breed, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
